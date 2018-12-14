236a502e2bb95c5072933514cd7ef918f9114b62
paladinlee <paladinlee@gmail.com>
2018-12-14 16:50:18 +0800

    完成範例練習

--- MVC_ActionFilters/App_Start/FilterConfig.cs
+++ MVC_ActionFilters/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using MVC_ActionFilters.Filters;
+using System.Web;
 using System.Web.Mvc;
 
 namespace MVC_ActionFilters
@@ -8,6 +9,9 @@ namespace MVC_ActionFilters
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+
+            //在 FilterConfig.cs 加上我們新寫的 Attribute 方法
+            filters.Add(new LogOutputAttribute());
         }
     }
 }
--- MVC_ActionFilters/Controllers/HomeController.cs
+++ MVC_ActionFilters/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC_ActionFilters.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace MVC_ActionFilters.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
